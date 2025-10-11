606c88f0c5a1338adeab4ed14dbbbc279fad3ec9
Malak Raef <malak.raef20@gmail.com>
2025-10-11 06:20:46 +0300

    feat: add achievements dialog fragment

---
+++ app/src/main/java/com/thechance/qurio/presentation/screen/achievements/AchievementsDialogFragment.kt
@@ -0,0 +1,85 @@
+package com.thechance.qurio.presentation.screen.achievements
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.thechance.qurio.data.local.AchievementsRepositoryImpl
+import com.thechance.qurio.databinding.DialogAchievementsBinding
+import com.thechance.qurio.domain.entity.Achievement
+import androidx.core.graphics.drawable.toDrawable
+import com.google.android.flexbox.AlignItems
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
+
+class AchievementsDialogFragment : DialogFragment(), AchievementsView {
+
+    private var _binding: DialogAchievementsBinding? = null
+    private val binding get() = _binding!!
+
+    private val presenter by lazy { AchievementsPresenter(AchievementsRepositoryImpl(requireContext())) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogAchievementsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerAchievements.adapter = AchievementsAdapter(emptyList()) {
+            presenter.onAchievementClicked(it)
+        }
+
+        binding.buttonOk.setOnClickListener { dialog?.hide() }
+        binding.buttonExit.setOnClickListener { dialog?.hide() }
+
+        presenter.attachView(this)
+        presenter.loadAchievements()
+    }
+
+    override fun showAchievements(achievements: List<Achievement>) {
+        val layoutManager = FlexboxLayoutManager(requireContext()).apply {
+            flexDirection = FlexDirection.ROW           // horizontal layout
+            justifyContent = JustifyContent.CENTER      // center items horizontally
+            alignItems = AlignItems.CENTER              // vertically centered
+            flexWrap = FlexWrap.WRAP                    // wrap to next line when full
+        }
+
+        binding.recyclerAchievements.layoutManager = layoutManager
+        binding.recyclerAchievements.adapter =
+            AchievementsAdapter(achievements) { presenter.onAchievementClicked(it) }
+    }
+
+    override fun showLoading() {}
+    override fun hideLoading() {}
+
+    override fun showError(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun openAchievementDetails(achievement: Achievement) {
+        showAchievementDialog(achievement)
+    }
+
+    private fun showAchievementDialog(achievement: Achievement) {
+        dismiss()
+
+        AchievementDescDialogFragment.newInstance(achievement)
+            .show(parentFragmentManager, "achievement_desc")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
