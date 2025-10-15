c8ce5f95ccb82c35c8dd61010bd4dd8188f14b7b
gehadepada <gehadepada@gmail.com>
2025-10-16 02:16:14 +0300

    feat:add CharacterPresenter.kt

---
+++ app/src/main/java/com/thechance/qurio/presentation/screen/characters/CharacterPresenter.kt
@@ -0,0 +1,37 @@
+package com.thechance.qurio.presentation.screen.characters
+
+import com.thechance.qurio.domain.entity.Character
+import com.thechance.qurio.domain.repository.CharactersRepository
+import com.thechance.qurio.presentation.base.BasePresenter
+import javax.inject.Inject
+
+class CharacterPresenter @Inject constructor(
+    private val charactersRepository: CharactersRepository
+): BasePresenter<CharacterView>() {
+    fun loadCharacters() {
+        tryToExecute(
+            callee = { charactersRepository.getAllCharacters() },
+            onStart = { view.showLoading() },
+            onSuccess = { characters -> view.showCharacters(characters) },
+            onFinish = { view.hideLoading() },
+            onError = { e -> view.showError(e.message ?: "Failed to load Characters") }
+        )
+    }
+
+    fun onCharacterClicked(character: Character) {
+        view.openCharacterDetails(character)
+    }
+
+    fun unlockCharacter(characterId: Int , isUnLocked : Boolean)  {
+        tryToExecute(
+            callee = {
+                charactersRepository.updateCharacterLockState(characterId , isUnLocked )
+                charactersRepository.getAllCharacters()
+            },
+            onSuccess = { updatedCharacters ->
+                view.showCharacters(updatedCharacters)
+            },
+            onError = { t -> view.showError(t.message ?: "Failed to unlock") }
+        )
+    }
+}
