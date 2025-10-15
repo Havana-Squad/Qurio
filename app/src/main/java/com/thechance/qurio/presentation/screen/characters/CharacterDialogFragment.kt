6b6509778d9fdd23e0bf073304f5af3ab1ff5150
gehadepada <gehadepada@gmail.com>
2025-10-16 02:17:56 +0300

    feat:add CharacterDialogFragment.kt

---
+++ app/src/main/java/com/thechance/qurio/presentation/screen/characters/CharacterDialogFragment.kt
@@ -0,0 +1,105 @@
+package com.thechance.qurio.presentation.screen.characters
+
+import android.app.Dialog
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.DialogFragment
+import com.google.android.flexbox.AlignItems
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
+import com.thechance.qurio.data.repository.CharactersRepositoryImpl
+import com.thechance.qurio.databinding.DialogCharactersBinding
+import com.thechance.qurio.domain.entity.Character
+
+class CharacterDialogFragment : DialogFragment(), CharacterView {
+
+    private var _binding: DialogCharactersBinding? = null
+    private val binding get() = _binding!!
+
+    private val presenter by lazy { CharacterPresenter(CharactersRepositoryImpl(requireContext())) }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+
+        dialog.window?.apply {
+            setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+            decorView.setPadding(0, 0, 0, 0)
+        }
+
+        return dialog
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            (resources.displayMetrics.widthPixels - (32 * resources.displayMetrics.density)).toInt(),
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogCharactersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.attachView(this)
+
+        binding.recyclerCharacters.adapter = CharacterAdapter(emptyList()) {
+            presenter.onCharacterClicked(it)
+        }
+
+        binding.buttonOk.setOnClickListener { dismiss() }
+        binding.buttonExit.setOnClickListener { dismiss() }
+
+        presenter.loadCharacters()
+    }
+
+    override fun showCharacters(Characters: List<Character>) {
+        val layoutManager = FlexboxLayoutManager(requireContext()).apply {
+            flexDirection = FlexDirection.ROW
+            justifyContent = JustifyContent.CENTER
+            alignItems = AlignItems.CENTER
+            flexWrap = FlexWrap.WRAP
+        }
+
+        binding.recyclerCharacters.layoutManager = layoutManager
+        binding.recyclerCharacters.adapter =
+            CharacterAdapter(Characters) { presenter.onCharacterClicked(it) }
+    }
+
+    override fun showLoading() {}
+    override fun hideLoading() {}
+
+    override fun showError(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun openCharacterDetails(Character: Character) {
+        showCharacterDialog(Character)
+    }
+
+    private fun showCharacterDialog(Character: Character) {
+        dismiss()
+        CharacterDescDialogFragment.newInstance(Character)
+            .show(parentFragmentManager, "Character_desc")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        presenter.detachView()
+        _binding = null
+    }
+}
