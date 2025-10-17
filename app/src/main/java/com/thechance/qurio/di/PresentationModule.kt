87fc69487b2c2b021afb543265dd800b0ecb2307
Hager Abobakr <hagarabobakr1@gmail.com>
2025-10-17 19:17:59 +0300

    feat:add OnboardingPresenter

--- app/src/main/java/com/thechance/qurio/di/PresentationModule.kt
+++ app/src/main/java/com/thechance/qurio/di/PresentationModule.kt
@@ -1,7 +1,9 @@
 package com.thechance.qurio.di
 
+import com.thechance.qurio.domain.repository.UserPreferencesRepository
 import com.thechance.qurio.domain.repository.game.GameRepository
 import com.thechance.qurio.presentation.screen.games_screen.GamesPresenter
+import com.thechance.qurio.presentation.screen.onboaeding.OnboardingPresenter
 import com.thechance.qurio.presentation.screen.played_games_screen.PlayedGamesPresenter
 import dagger.Module
 import dagger.Provides
@@ -9,13 +11,13 @@ import javax.inject.Singleton
 
 @Module
 class PresentationModule {
-    companion object{
+    companion object {
         @Provides
         @Singleton
         fun provideGamePresenter(
             gameRepository: GameRepository
         ): GamesPresenter {
-            return GamesPresenter( gameRepository)
+            return GamesPresenter(gameRepository)
         }
 
         @Provides
@@ -24,6 +26,14 @@ class PresentationModule {
             gameRepository: GameRepository
         ): PlayedGamesPresenter {
             return PlayedGamesPresenter(gameRepository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideOnboardingPresenter(
+            userPreferences: UserPreferencesRepository
+        ): OnboardingPresenter {
+            return OnboardingPresenter(userPreferences)
         }
     }
 }
