23c81f50bf484094efc78e666b34f6726800a58c
gehadepada <gehadepada@gmail.com>
2025-10-17 20:47:22 +0300

    feat:add user repo interface

---
+++ app/src/main/java/com/thechance/qurio/domain/repository/UserRepository.kt
@@ -0,0 +1,13 @@
+package com.thechance.qurio.domain.repository.user
+
+import com.thechance.qurio.domain.entity.Character
+
+interface UserRepository {
+    suspend fun getUserCharacter(): Character
+    suspend fun updateUserCharacter(character: Character)
+    suspend fun getUserStatistics(): Triple<Int, Int, Int>
+    suspend fun getUserStreak(): Int
+    suspend fun updateStreak(streak: Int)
+    suspend fun incrementStreak()
+    suspend fun addPoints(points: Int)
+}
