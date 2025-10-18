b0ed28ac38ade4b13c9283e801c43bb66a03fac3
edrees <muhammed.edrees101@gmail.com>
2025-10-18 18:36:14 +0300

    feat: implement streak management logic in StartPlayFragment and UserRepository

--- app/src/main/java/com/thechance/qurio/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/thechance/qurio/data/repository/UserRepositoryImpl.kt
@@ -1,14 +1,26 @@
 package com.thechance.qurio.data.repository
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.thechance.qurio.data.local.CharactersDataSource
 import com.thechance.qurio.data.local.UserDataSource
 import com.thechance.qurio.domain.entity.Character
 import com.thechance.qurio.domain.repository.user.UserRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val context: Context,
+    private val preferences: DataStore<Preferences>
 ) : UserRepository {
 
 
@@ -25,15 +37,31 @@ class UserRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getUserStreak(): Int {
-        return UserDataSource.getUserStreak()
+        return preferences.data.map {
+            it[KEY_CURRENT_STREAK] ?: 0
+        }.first()
     }
 
-    override suspend fun updateStreak(streak: Int) {
-        UserDataSource.updateStreak(streak)
+    override suspend fun getLastPlayedDate(): LocalDate {
+        return preferences.data.map {
+            it[KEY_LAST_PLAYED_DATE]?.let { dateString ->
+                LocalDate.parse(dateString)
+            } ?: LocalDate(1970, 1, 1)
+        }.first()
+    }
+
+    override suspend fun resetStreak() {
+        preferences.edit {
+            it[KEY_CURRENT_STREAK] = 1
+            it[KEY_LAST_PLAYED_DATE] = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date.toString()
+        }
     }
 
     override suspend fun incrementStreak() {
-        UserDataSource.incrementStreak()
+        preferences.edit {
+            it[KEY_CURRENT_STREAK] = (it[KEY_CURRENT_STREAK] ?: 0) + 1
+            it[KEY_LAST_PLAYED_DATE] = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date.toString()
+        }
     }
 
     override suspend fun addPoints(points: Int) {
@@ -52,4 +80,8 @@ class UserRepositoryImpl @Inject constructor(
         UserDataSource.updateAwards(awards)
     }
 
+    private companion object {
+        val KEY_CURRENT_STREAK = intPreferencesKey("current_streak")
+        val KEY_LAST_PLAYED_DATE = stringPreferencesKey("last_played_date")
+    }
 }
